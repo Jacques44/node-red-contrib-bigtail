7ae5c10b3e8e21831feed90009990b5dddc4d21f
Jacques W <jacquesn4444@gmail.com>
2016-05-22 14:56:46 +0200

    First work

--- bigtail/bigtail.js
+++ bigtail/bigtail.js
@@ -39,6 +39,7 @@ module.exports = function(RED) {
    // Definition of which options are known for spawning a command (ie node configutation to BigExec.spawn function)
   var tail_options = {
     "size": 0,
+    "size_kbyte": 0,
     "add_cr": false
   }  
 
@@ -50,18 +51,43 @@ module.exports = function(RED) {
 
       var buffer = [];
       var size = config.size || 0;
-      if (size < 0) size = 0;
+      if (size < 0) throw new Error("Funny... :(");
+
+      var size_kbyte = config.size_kbyte || 0;
+      if (size_kbyte < 0) throw new Error("Funny... :(");
+
       var add_cr = config.add_cr ? "\n" : "";
+      var total_size = 0;
+      var trashed = 0;
+      var total_messages = 0;
+
+      var trash_first = function() {
+        var trash = buffer.shift();
+        total_size -= trash.length;
+        trashed++;
+        return trash;       
+      }
 
       // Create a buffer stream of n payloads
       var outstream = new stream.Transform( { objectMode: true });
       outstream._transform = function(data, encoding, done) {
 
-        buffer.push(data + add_cr);
-        if (size > 0 && buffer.length > size) buffer.shift();
+        total_messages++;
 
+        buffer.push(data + add_cr); 
+        total_size += data.length + add_cr.length;
+
+        if (size > 0 && buffer.length > size) trash_first();
+
+        if (size_kbyte > 0) while (total_size > size_kbyte) {
+          // If no more items, get out
+          if (! trash_first()) break;
+        }
+
+        this.stats({ trashed: trashed, total_messages: total_messages });
+        
         done();
-      };
+      }.bind(this);
 
       outstream._flush = function(done) {
         if (buffer.length == 0) return done();
@@ -73,11 +99,14 @@ module.exports = function(RED) {
 
     }
 
+    var progress = function(running) { return (this._runtime_control.trashed || 0) + " trashed over " + (this._runtime_control.total_messages || 0) }
+
     // new instance of biglib for this node
     var bignode = new biglib({ 
       config: config, node: this,   // biglib needs to know the node configuration and the node itself (for statuses and sends)
       status: 'filesize',           // define the kind of informations displayed while running
       parser_config: tail_options,  // the parser configuration (ie the known options the parser will understand)
+      status: progress,
       parser: tail                  // the parser 
     });
 
